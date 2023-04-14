de29853216b4222d8289903889735f3d012621bf
ynr <ya_onur@hotmail.com>
2023-04-14 05:10:05 +0300

    formatted tests

--- tests/test_filter_url.py
+++ tests/test_filter_url.py
@@ -4,27 +4,25 @@ from crawler import Crawler
 
 @pytest.mark.asyncio
 async def test_filter_url():
-	domain = "sample.com"
-	wc = Crawler(domain)
-	# dont think there will be anchor tag with www.sdfdsf.com without slash
-	# maybe theres some cases like that
-	# if so filter_url should be changed to handle that
+    domain = "sample.com"
+    wc = Crawler(domain)
+    # dont think there will be anchor tag with www.sdfdsf.com without slash
+    # maybe theres some cases like that
+    # if so filter_url should be changed to handle that
 
-	tests_list = [
-	{"anchor":"https://sample.com/","result":"https://sample.com/"},
-	{"anchor":"/subpage", "result":"https://sample.com/subpage"},
-	{"anchor":"subpage", "result":None},
-	{"anchor":"https://www.sample.com", "result":"https://www.sample.com"},
-	{"anchor":"//www.sample.com", "result":"https://www.sample.com"},
-	{"anchor":"http://www.sample.com", "result":"http://www.sample.com"},
-	{"anchor":"ftp://www.sample.com", "result":None},
-	{"anchor":"sample", "result":None},
-	{"anchor":"http://sample", "result":None},
-	{"anchor":"http://.com", "result":None},
-]
+    tests_list = [
+        {"anchor": "https://sample.com/", "result": "https://sample.com/"},
+        {"anchor": "/subpage", "result": "https://sample.com/subpage"},
+        {"anchor": "subpage", "result": None},
+        {"anchor": "https://www.sample.com", "result": "https://www.sample.com"},
+        {"anchor": "//www.sample.com", "result": "https://www.sample.com"},
+        {"anchor": "http://www.sample.com", "result": "http://www.sample.com"},
+        {"anchor": "ftp://www.sample.com", "result": None},
+        {"anchor": "sample", "result": None},
+        {"anchor": "http://sample", "result": None},
+        {"anchor": "http://.com", "result": None},
+    ]
 
-	for test in tests_list:
-		result = await wc.filter_url(domain, test["anchor"])
-		assert result == test["result"]
-
-	
+    for test in tests_list:
+        result = await wc.filter_url(domain, test["anchor"])
+        assert result == test["result"]
--- tests/test_request.py
+++ tests/test_request.py
@@ -1,30 +1,32 @@
 import pytest
-import httpx
 from crawler import Crawler
+
 
 @pytest.fixture
 async def crawler():
-	domain = "httpbin.org"
-	yield  Crawler(domain)
+    domain = "httpbin.org"
+    yield Crawler(domain)
 
 
 @pytest.mark.asyncio
 async def test_request_not_found(crawler):
-	async for c in crawler:
-		url = "https://httpbin.org/status/404"
-		response	=await c._request(url)
-		assert response is ""
+    async for c in crawler:
+        url = "https://httpbin.org/status/404"
+        response = await c._request(url)
+        assert response is ""
+
 
 @pytest.mark.asyncio
 async def test_request_successful(crawler):
-	async for c in crawler:
-		url = "https://httpbin.org/delay/get"
-		response = await c._request(url)
-		assert response is not None
+    async for c in crawler:
+        url = "https://httpbin.org/delay/get"
+        response = await c._request(url)
+        assert response is not None
+
 
 @pytest.mark.asyncio
 async def test_request_timeout(crawler):
-	async for c in crawler:
-		url = "https://httpbin.org/delay/60"
-		response=await c._request(url)
-		assert response is None
+    async for c in crawler:
+        url = "https://httpbin.org/delay/60"
+        response = await c._request(url)
+        assert response is None
